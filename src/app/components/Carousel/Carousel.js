fdf83aa565e2be49adf366defe9df7c7affc679a
Mauricio Munoz <mauripedroza89@gmail.com>
2022-05-16 14:01:35 -0500

    location updated

--- src/app/components/Carousel/Carousel.js
+++ src/app/components/Carousel/Carousel.js
@@ -13,6 +13,7 @@ import image2 from '../../assets/images/freshdrinks.jpeg';
 import image3 from '../../assets/images/food/wings.jpeg';
 import image4 from '../../assets/images/food/carousel2.jpeg';
 import image5 from '../../assets/images/food/carousel3.jpeg';
+import image6 from '../../assets/images/mariachistruck.jpeg';
 
 
   function Carousels() {
@@ -23,6 +24,54 @@ import image5 from '../../assets/images/food/carousel3.jpeg';
                            <div className='container-fluid' style={{ display: 'block', height: 900, padding: 0 }}>  
                            <Carousel >  
                            <Carousel.Item > 
+                                <Box
+                                    role={'group'}
+                                    p={6}
+                                    w={'full'}
+                                    bg={useColorModeValue('white', 'gray.800')}
+                                    boxShadow={'2xl'}
+                                    rounded={'lg'}
+                                    pos={'relative'}
+                                    zIndex={1}>
+                                    <Box
+                                        align={'center'}
+                                        rounded={'lg'}
+                                        borderRadius='full'
+                                        mt={-12}
+                                        pos={'relative'}
+                                        height={{ base: '400', md: '700' }}
+                                        _after={{
+                                        transition: 'all .3s ease',
+                                        content: '""',
+                                        w: 'full',
+                                        h: 'full',
+                                        pos: 'absolute',
+                                        top: 5,
+                                        left: 0,
+                                        backgroundImage: `url(${image6})`,
+                                        filter: 'blur(15px)',
+                                        zIndex: -1,
+                                        }}
+                                        _groupHover={{
+                                        _after: {
+                                            filter: 'blur(50px)',
+                                        },
+                                        }}>
+                                        <Image
+                                        align={'center'}
+                                        rounded={'lg'}
+                                        maxW={'100%'}
+                                        maxH={'100%'}
+                                        objectFit={'cover'}
+                                        src={image6}
+                                        />
+                                    </Box>
+                                    <Carousel.Caption>
+                                <Heading textShadow="2px 2px #ff0000" > Because we love our clients, we have moved to a more confortable place, visit us to our new location at 12046 Potranco Rd, San Antonio, TX 78245, next to the Lone Star Crossfit</Heading>
+                                    </Carousel.Caption>
+                                    </Box>
+                                </Carousel.Item>
+                                <Carousel.Item > 
                                 <Box
                                     role={'group'}
                                     p={6}
--- src/app/components/Contact/Contact.js
+++ src/app/components/Contact/Contact.js
@@ -23,7 +23,7 @@ import { SiDoordash } from "react-icons/si";
             align={'center'}
             direction={{ base: 'column', md: 'row' }}
             spacing={{ base: 10, md: 4, lg: 10 }}>
-       <Link href='https://goo.gl/maps/rPZHwA1gc3ftdWGm6'>         
+       <Link href='https://goo.gl/maps/WszpoLcvKMge1Jtq8'>         
         <Box
           maxW={'320px'}
           w={'100,500,full'}
@@ -43,7 +43,7 @@ import { SiDoordash } from "react-icons/si";
             textAlign={'center'}
             color={useColorModeValue('#f2f4f3', 'red.400')}
             px={3}>
-            11922 Potranco Rd, San Antonio, TX 78253
+            12046 Potranco Rd, San Antonio, TX 78245
           </Text>
         </Box>
         </Link>
