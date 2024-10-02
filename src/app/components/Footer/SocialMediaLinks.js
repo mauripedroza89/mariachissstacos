7c1f13a3bcc119d8ecaf6a4754390b597a8ff774
Mauricio Pedroza <79674181+mauripedroza89@users.noreply.github.com>
2024-10-02 15:56:06 -0500

    Update SocialMediaLinks.js

--- src/app/components/Footer/SocialMediaLinks.js
+++ src/app/components/Footer/SocialMediaLinks.js
@@ -7,7 +7,7 @@ export const SocialMediaLinks = (props) => (
     <IconButton as="a" href="https://www.instagram.com/mariachisss_tacos/" aria-label="Instagram" icon={<FaInstagramSquare fontSize="30px" />} />
     <IconButton as="a" href="https://www.facebook.com/Mariachisss-tacos-113314177519736/" aria-label="Facebook" icon={<FaFacebookSquare fontSize="30px" />} />
     <IconButton as="a" href="https://www.yelp.com/biz/mariachisss-tacos-san-antonio-3" aria-label="Yelp" icon={<FaYelp fontSize="30px" />} />
-    <IconButton as="a" href="https://g.page/area51foodpark?share" aria-label="Maps" icon={<FaMapMarkerAlt fontSize="30px" />} />
+    <IconButton as="a" href="https://maps.app.goo.gl/Zw4itsUVaZPfzw7f8" aria-label="Maps" icon={<FaMapMarkerAlt fontSize="30px" />} />
 
   </ButtonGroup>
 )
